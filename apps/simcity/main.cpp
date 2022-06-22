7683f950c5a767c31368fd8b53e39da1c4317507
Semeon Valgushev <semeon.valgushev@gmail.com>
2022-06-21 20:02:49 -0500

    +playground class and entities

--- apps/simcity/main.cpp
+++ apps/simcity/main.cpp
@@ -19,32 +19,98 @@ class RandomManager
     RandomManager()  {}
 };
 
-double main_VectorMean(const VECTOR<double> &vec)
+class Entity;
+
+class Playground
 {
-  double mean = 0;
-  int vec_size = vec.size();
+private:
+  uint dim;
+  VECTOR<double> size;
+  VECTOR<SHARED_PTR<Entity>> entities;
 
-  for(int i = 0; i < vec_size; i++)
-    mean += vec[i] / vec_size;
-
-  return mean;
-}
-
-double main_VectorSigma(const VECTOR<double> &vec)
-{
-  double sigma = 0;
-  int vec_size = vec.size();
-
-  if (vec_size > 1)
+public:
+  // Игровая площадка является тором 1х1х1...
+  Playground(uint dim) : dim(dim), size(dim)
   {
-    double mean = main_VectorMean(vec);
-
-    for(int i = 0; i < vec_size; i++)
-      sigma += (vec[i] - mean) * (vec[i] - mean) / (vec_size - 1);
+    for(uint i = 0; i < dim; i++)
+      size[i] = 1;
   }
 
-  return sqrt(sigma);
-}
+  // Расстояние между точками на торе
+  double Distance(VECTOR<double> &x1, VECTOR<double> &x2) const
+  {
+    double r = 0;
+
+    for(uint i = 0; i < dim; i++)
+    {
+      double r1 = U1Coordinate(x1[i] - x2[i], size[i]);
+      r1 = (r1 > size[i] / 2) ? size[i] - r1 : r1;
+
+      r += r1 * r1;
+    }
+
+    return std::sqrt(r);
+  }
+
+  // Размерность ирового тора
+  uint Dim() const { return dim; }
+
+  // Добавить сущность на тор
+  void AddEntity(Entity &ent)
+  {
+    entities.emplace_back(&ent);
+  }
+
+  // Вспомогательная функция для округления координаты на S^1
+  static double U1Coordinate(double x, double l)
+  {
+    double rem = std::fmod(x, l);
+    return (rem >= 0) ? rem : l + rem;
+  }
+};
+
+// Абстрактный базовый класс для различных типов сущностей
+// Привязан к игровой площадке
+// Имеет координату и внутреннее время жизни
+class Entity
+{
+public:
+  SHARED_PTR<Playground> pg_ptr;
+  VECTOR<double> x;
+  double t;
+
+  Entity(Playground &pg) : pg_ptr(&pg), x(pg.Dim()), t(0)
+  {
+    pg.AddEntity(*this);
+  }
+};
+
+// Движущаяся сущность, имеет скорость v
+class MovingEntity : Entity
+{
+public:
+  VECTOR<double> v;
+
+  MovingEntity(Playground &pg) : Entity(pg), v(pg.Dim()) {}
+};
+
+// Съедобная сущность, имеет массу (питательность) m
+class EdibleEntity : Entity
+{
+public:
+  double m;
+
+  EdibleEntity(Playground &pg) : Entity(pg), m(0) {}
+  EdibleEntity(Playground &pg, double m) : Entity(pg), m(m) {}
+};
+
+// Просто трава
+class Weed : EdibleEntity
+{
+//public:
+//  Weed(Playground &pg) : EdibleEntity(pg) {}
+//  Weed(Playground &pg, double m) : EdibleEntity(pg, m) {}
+};
 
 void main_PrintVector(FILE *f, VECTOR<int> &vec)
 {
@@ -93,6 +159,16 @@ int main(int argc, char **argv)
   val = f1.FunctionValue(0.344);
   pStdLogs.Write("VALUE: %2.15le\n", val);
 
+  Playground pg(2);
+  pStdLogs.Write("REMINDER: %2.15le\n", Playground::U1Coordinate(-1.34, 1.3));
+  VECTOR<double> x1 = {0, 0.13};
+  VECTOR<double> x2 = {0, 0.13};
+  for(int i = 0; i < 100; i++)
+  {
+    double r = pg.Distance(x1, x2);
+    pStdLogs.Write("%2.15le\n", r);
+    x2[1] -= 0.05;
+  }
 
   //FILE *f_m_out = pDataDir.OpenFile("test.bin", f_bin_write_attr);
 
